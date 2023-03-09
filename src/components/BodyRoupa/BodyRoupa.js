f58fdd13a9009b562042b30f0fb1a727d4d6be28
gabrielts212 <gabrielts2122@gmail.com>
2023-03-09 01:00:33 -0300

    ajustments

--- src/components/BodyRoupa/BodyRoupa.js
+++ src/components/BodyRoupa/BodyRoupa.js
@@ -1,35 +1,96 @@
 import React from 'react'
-import { Flex,Box,h1,Heading,Button} from '@chakra-ui/react'
+import { Flex,Box,h1,Heading,Button,Text,SimpleGrid } from '@chakra-ui/react'
 import Image from "next/image";
 import Logo from "../../assets/sapatos.png";
+import Logoo from "../../assets/sapato2.png";
+import Logooo from "../../assets/sapato3.png";
+import Logoooo from "../../assets/sapato4.png";
 
 function BodyRoupa() {
   return (
     <div>
+
+
 <Flex  justify="center" color="white" bg="#C2185B" pt="5rem">
 
 
 <Heading>30% OFF</Heading>
 </Flex>
 
+<Flex justify="center" bg="#C2185B"h="662px">
+
+
+<SimpleGrid columns={[1, 2, 4]} h="662px" justify="center" gap="20" align="center"pt="5rem">
+  
+<Box  h="388px" w=" 260px"bg="white" >
+ <Box bg="white" >
+<Image src={Logo} alt="" h="757.94px"   w="600px" />
+<Text fontSize='lg'>SCARPIN SALTO FINO BÁSICO</Text>
+<Text fontSize='lg'>R$140,00 </Text>
+
+ </Box>
+  </Box>
+  <Box  h="388px" w=" 260px"bg="white" >
+ <Box bg="white" >
+<Image src={Logoo} alt="" h="757.94px"   w="600px" />
+<Text fontSize='lg'>SCARPIN SALTO FINO BÁSICO</Text>
+<Text fontSize='lg'>R$140,00 </Text>
+
+ </Box>
+  </Box>
+  <Box  h="388px" w=" 260px"bg="white" >
+ <Box bg="white" >
+<Image src={Logooo} alt="" h="757.94px"   w="600px" />
+<Text fontSize='lg'>SCARPIN SALTO FINO BÁSICO</Text>
+<Text fontSize='lg'>R$140,00 </Text>
+
+ </Box>
+  </Box>
+  <Box  h="388px" w=" 260px"bg="white" >
+ <Box bg="white" >
+<Image src={Logoooo} alt="" h="757.94px"   w="600px" />
+<Text fontSize='lg'>SCARPIN SALTO FINO BÁSICO</Text>
+<Text fontSize='lg'>R$140,00 </Text>
+
+ </Box>
+  </Box>
+</SimpleGrid>
+</Flex>
+
+{/* 
 <Flex  bg="#C2185B" h="662px" justify="center" gap="20" align="center">
 
+  <Box  h="388px" w=" 260px"bg="white" >
+ <Box bg="white" >
+<Image src={Logo} alt="" h="757.94px"   w="600px" />
+<Text fontSize='lg'>SCARPIN SALTO FINO BÁSICO</Text>
+<Text fontSize='lg'>R$140,00 </Text>
 
- 
-  <Flex  h="388px" w=" 260px" >
-<Image src={Logo} alt="" h="757.94px"   w="600px" />
-  </Flex>
-  <Flex  h="388px" w=" 260px" >
-<Image src={Logo} alt="" h="757.94px"   w="600px" />
-  </Flex>
-  <Flex  h="388px" w=" 260px" >
-<Image src={Logo} alt="" h="757.94px"   w="600px" />
-  </Flex>
-  <Flex  h="388px" w=" 260px" >
-<Image src={Logo} alt="" h="757.94px"   w="600px" />
-  </Flex>
- 
-</Flex>
+ </Box>
+  </Box>
+  <Box  h="388px" w=" 260px"bg="white" >
+ <Box bg="white" >
+<Image src={Logoo} alt="" h="757.94px"   w="600px" />
+<Text fontSize='lg'>SCARPIN SALTO FINO BÁSICO</Text>
+<Text fontSize='lg'>R$140,00 </Text>
+ </Box>
+  </Box>
+  <Box  h="388px" w=" 260px"bg="white" >
+ <Box bg="white" >
+<Image src={Logooo} alt="" h="757.94px"   w="600px" />
+<Text fontSize='lg'>SCARPIN SALTO FINO BÁSICO</Text>
+<Text fontSize='lg'>R$140,00 </Text>
+ </Box>
+  </Box>
+  <Box  h="388px" w=" 260px"bg="white" >
+ <Box bg="white" >
+<Image src={Logoooo} alt="" h="757.94px"   w="600px" />
+<Text fontSize='lg'>SCARPIN SALTO FINO BÁSICO</Text>
+<Text fontSize='lg'>R$140,00 </Text>
+ </Box>
+  </Box>
+
+</Flex> */}
 
  
     </div>
